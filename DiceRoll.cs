c259593ea900e0ea4c000d753cf5335e3ef02e59
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2023-11-13 19:50:12 -0800

    Add DiceRoll, Update Program

---
+++ DiceRoll.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog_226_FunctionalProgramming
+{
+    public static class DiceRoll
+    {
+        static Random rand = new Random();
+
+        public static int RollTheDie(int number)
+        {
+            return rand.Next(1, number + 1);
+        } 
+
+        public static int FunctionalDieRoll(this int number)
+        {
+            return rand.Next(1, number + 1);
+        }
+
+        public static double Add(double num1, double num2) { return num1 + num2; }
+
+        public static double AddFunc(this double num1, double num2) { return num1 + num2; }
+
+        // Extension Method
+        // Static Class
+        // static method
+        // this
+
+        // A number then adding loop, and it will loop that many times
+        // Take a delegate
+
+        // Predicate
+        // Func
+        // 
+
+        public static void Loop(this int number, Action display )
+        {
+            for (int i = 0; i < number; i++)
+            {
+                display();
+            }
+        }
+
+    } // class
+
+} // namespace
--- Program.cs
+++ Program.cs
@@ -4,6 +4,68 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+
+        //int dieRollOOP = DiceRoll.RollTheDie(6);
+        //int dieRollFunc = 5000.FunctionalDieRoll();
+
+        //double addTwoNumbersOOp = DiceRoll.Add(1, 2);
+        //double addTwoFunc = 200.0.AddFunc(3);
+
+        ////1.AddFunc(2);
+        ////1.0.AddFunc(2);
+
+        //Item iphone = new Item("Iphone 15", 10, 2000);
+        //Item updated = iphone.UpdateStock(200);
+
+        //iphone.UpdateStock(300).DisplayItem();
+
+        12.Loop(DisplayCat);
+
+
+        //Item updateIphone = new Item(iphone.Name, iphone.Stock + 10, iphone.Price);
+
+
+
+
+        Console.WriteLine();
+
+
+
+        //Console.WriteLine(dieRollFunc);
+
+        //int dieRoll = 20;
+        //int timesLooped = 5000;
+
+        //int[] countOfNumbers = new int[dieRoll];
+
+
+        //for (int i = 0; i < timesLooped; i++)
+        //{
+        //    int currentNumber = DiceRoll.RollTheDie(dieRoll);
+        //    Console.WriteLine($"Roll the Die: {i} - {currentNumber}");
+
+        //    countOfNumbers[currentNumber - 1]++;
+        //}
+
+        //for (int i = 0; i < countOfNumbers.Length; i++)
+        //{
+        //    double numberOfTimes = countOfNumbers[i];
+        //    double percentage = numberOfTimes / timesLooped;
+
+        //    Console.WriteLine($"Number {i + 1} rolled - {numberOfTimes} times. {(double)percentage}");
+        //}
+
+
     }
+
+    public static void Display()
+    {
+        Console.WriteLine($"Number");
+    }
+
+    public static void DisplayCat()
+    {
+        Console.WriteLine($"Cat");
+    }
+
 }
