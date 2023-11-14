70b117d5e2ea7574a1de4564eb6cc8e9da50c784
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2023-11-13 19:50:48 -0800

    Add Item, ItemFunction, .sln

---
+++ Item.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog_226_FunctionalProgramming
+{
+    public class Item
+    {
+
+        string _name;
+        int _stock;
+        double _price;
+
+        public Item(string name, int stock, double price)
+        {
+            _name = name;
+            _stock = stock;
+            _price = price;
+        }
+
+        public string Name { get => _name; set => _name = value; }
+        public int Stock { get => _stock; set => _stock = value; }
+        public double Price { get => _price; set => _price = value; }
+
+
+
+    }
+
+
+
+}
---
+++ ItemFunctions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog_226_FunctionalProgramming
+{
+    public static  class ItemFunctions
+    {
+
+        // Update the stock, but not effect the original item
+        public static Item UpdateStock(this Item item, int newStock)
+        {
+            string name = item.Name;
+            int stock = newStock;
+            double price = item.Price;
+
+            return new Item(name, stock, price);
+
+        }
+
+        public static void DisplayItem(this Item item)
+        {
+            Console.WriteLine($"Name: {item.Name}\n");
+            Console.WriteLine($"Stock: {item.Stock}\n");
+            Console.WriteLine($"Price: {item.Price}\n");
+            Console.WriteLine($"\n");
+        }
+
+    }
+}
